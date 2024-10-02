30874afcf9a381544c97f835dd4ea87f89df1aa0
Matheus-Morais <matheus2992morais@gmail.com>
2024-10-02 11:19:03 -0300

    Adiciona uma nova row com o botão 0 e reorganiza os botões

--- src/App.js
+++ src/App.js
@@ -101,27 +101,30 @@ const App = () => {
       <Content>
         <Input value={currentNumber}/>
         <Row>
-          <Button label="x" onClick={handleMultiplyNumbers}/>
-          <Button label="/" onClick={handleDivideNumbers}/>
           <Button label="c" onClick={handleOnClear}/>
-          <Button label="."/>
+          <Button label="/" onClick={handleDivideNumbers}/>
         </Row>
         <Row>
           <Button label="7" onClick={() => handleAddNumber('7')}/>
           <Button label="8" onClick={() => handleAddNumber('8')}/>
           <Button label="9" onClick={() => handleAddNumber('9')}/>
-          <Button label="-" onClick={handleMinusNumbers}/>
+          <Button label="x" onClick={handleMultiplyNumbers}/>
         </Row>
         <Row>
           <Button label="4" onClick={() => handleAddNumber('4')}/>
           <Button label="5" onClick={() => handleAddNumber('5')}/>
           <Button label="6" onClick={() => handleAddNumber('6')}/>
-          <Button label="+" onClick={handleSumNumbers}/>
+          <Button label="-" onClick={handleMinusNumbers}/>
         </Row>
         <Row>
           <Button label="1" onClick={() => handleAddNumber('1')}/>
           <Button label="2" onClick={() => handleAddNumber('2')}/>
           <Button label="3" onClick={() => handleAddNumber('3')}/>
+          <Button label="+" onClick={handleSumNumbers}/>
+        </Row>
+        <Row>
+          <Button label="0" onClick={() => handleAddNumber('0')}/>
+          <Button label="."/>
           <Button label="=" onClick={handleEquals}/>
         </Row>
       </Content>
